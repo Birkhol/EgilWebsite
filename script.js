d51010b36ffcb91274f624d047e96d9b4d93c249
SanderBirkhol <sanderbirkhol@gmail.com>
2025-11-03 21:54:56 +0100

    Added a hamburgermenu to the navbar, fixed scaling in index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
